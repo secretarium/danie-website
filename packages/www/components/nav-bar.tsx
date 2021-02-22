9518a062965651802560d360bfa924983110da55
Florian Guitton <florian@secretarium.com>
2021-02-22 21:43:32 +0000

    chore: Add app login browsing button

--- packages/www/components/nav-bar.tsx
+++ packages/www/components/nav-bar.tsx
@@ -12,10 +12,15 @@ const NavBar: React.FC<NavBarProps> = ({ fixedToggle }) => {
     return (
         <div className={`${navBarStyles.navBarWrapper} ${fixedToggle ? '' : navBarStyles.fixedToggle} overflow-visible`}>
             <div className={`container mx-auto px-5 md:text-center ${navBarStyles.navBar}`}>
-                <nav className="flex items-center flex-wrap text-center">
+                <nav className="flex items-center flex-wrap text-center relative">
                     <Link href="/" >
                         <a href="/" className={`sm:pl-2 z-10 flex-grow ${navBarStyles.logoWrapper} `}>
                             <img src={DANIELogo} alt="Secretarium" className="inline-block z-10" />
+                        </a>
+                    </Link>
+                    <Link href="/app">
+                        <a href="/app" className={`sm:pl-2 z-10 absolute right-0 ${navBarStyles.logoWrapper} `}>
+                            Log in
                         </a>
                     </Link>
                 </nav>
